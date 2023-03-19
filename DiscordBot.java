c2ea02b1dea84ec6128e5b43bacb621990ca3eda
Előd Sipos <127198576+elod-sipos@users.noreply.github.com>
2023-03-19 10:18:55 +0000

    Create DiscordBot.java

---
+++ DiscordBot.java
@@ -0,0 +1,22 @@
+import events.*;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class DiscordBot {
+    public static void main(String[] args) throws LoginException {
+
+        final String TOKEN = "";
+        JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
+        JDA jda = jdaBuilder
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES)
+                .addEventListeners(new ReadyEventListener(), new MessageEventListener(), new InteractionEventListener())
+                .build();
+
+        jda.upsertCommand("greet", "says hello").setGuildOnly(true).queue();
+        jda.upsertCommand("godspeed", "says goodbye").setGuildOnly(true).queue();
+    }
+
+}
